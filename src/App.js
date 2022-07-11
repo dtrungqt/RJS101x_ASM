a07320752780c41c94e1e46160cf5e5b89136882
DTrung <ngdinhtrung.qt@gmail.com>
2022-07-11 14:51:35 +0700

    8th update UI 4th:add  nav and footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+// import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
 import StaffList from "./components/StaffListComponent";
 import { STAFFS } from "./shared/staffs";
@@ -17,14 +17,42 @@ class App extends Component {
     return (
       <div>
         {/* Navbar  */}
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ứng dụng quản lý nhân sự</NavbarBrand>
-          </div>
-        </Navbar>
+        <nav className="navbar navbar-expand-sm bg-primary navbar-dark mb-5">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link font-weight-bold" href="/">
+                Nhân viên
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link font-weight-bold" href="/">
+                Chỉnh sửa
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link font-weight-bold" href="/">
+                Tìm kiếm
+              </a>
+            </li>
+          </ul>
+        </nav>
 
         {/* Hiển thị danh sách nhân viên  */}
         <StaffList staffs={this.state.staffs} />
+
+        {/* FOOTER  */}
+        <footer>
+          <div className="jumbotron bg-primary mt-5">
+            <div className="col-12">
+              <p>
+                <strong>Nguyễn Đình Trung FX16739</strong>
+              </p>
+              <p>Liên Chiểu, Đà Nẵng, Việt Nam</p>
+              <p>SĐT: 0123456789</p>
+              <p>Gmail: trungndfx@gmail.com</p>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
